9b58ee5fea89502d28cca0c3aec68697653f11f0
PhanHoangLocAG <phanhoangloc2017@gmail.com>
2020-11-13 08:21:29 +0700

    Them file goc

---
+++ source.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int hieu(int a,int b)
+int tong(int a,int b)
+int tich(int a,int b)
+int thuong(int a,int b)
+int main()
+{
+	int a, b;
+	cout << "DAY LA CHUONG TRINH DE THUC HANH GIT" << endl;
+	cout << "nhap a"<< endl; 
+	cin >> a;
+	cout << "nhap b" << endl;
+	cin >> b;
+	system("pause");
+	cout<<"hello word";
+	return 0;
+}
